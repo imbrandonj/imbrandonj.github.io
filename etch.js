cfd95a3f35320a87e007ce7ac63b3538b9001370
Brandon Jenkins <90408885+imbrandonj@users.noreply.github.com>
2022-03-31 20:59:23 -0500

    Add files via upload

--- etch.js
+++ etch.js
@@ -7,17 +7,24 @@ function changecolor(el) {
 
 const gridContainer = document.querySelector('.gridContainer');
 
-const EIGHT_EIGHT = () => {
+const RESET_GRID = () => {
+    gridContainer.classList.remove('eight_eight');
+    gridContainer.classList.remove('sixteen_sixteen');
+    gridContainer.classList.remove('thirtytwo_thirtytwo');
+    gridContainer.classList.remove('sixtyfour_sixtyfour');
 
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
-    } // This resets the grid
+    }  // This resets the grid
+}
+
+const EIGHT_EIGHT = () => {
 
     gridContainer.classList.add('eight_eight');  // css class style for grid
 
     for (let numberOfGrids = 1; numberOfGrids <= 8*8; numberOfGrids++) {
         const gridBox = document.createElement('div');
-        gridBox.classList.add('gridBox');
+        gridBox.classList.add('gridBox');  // adds individual grid boxes
 
         gridBox.addEventListener('mouseover', function handleMouseOver() {
             gridBox.style.backgroundColor = chosenColor;
@@ -31,15 +38,11 @@ const EIGHT_EIGHT = () => {
 
 const SIXTEEN_SIXTEEN = () => {
 
-    while (gridContainer.firstChild) {
-        gridContainer.removeChild(gridContainer.firstChild);
-    }  // This resets the grid
-
     gridContainer.classList.add('sixteen_sixteen');  // css class style for grid
 
     for (let numberOfGrids = 1; numberOfGrids <= 16*16; numberOfGrids++) {
         const gridBox = document.createElement('div');
-        gridBox.classList.add('gridBox');
+        gridBox.classList.add('gridBox');  // adds individual grid boxes
 
         gridBox.addEventListener('mouseover', function handleMouseOver() {
             gridBox.style.backgroundColor = chosenColor;
@@ -53,15 +56,11 @@ const SIXTEEN_SIXTEEN = () => {
 
 const THIRTYTWO_THIRTYTWO = () => {
 
-    while (gridContainer.firstChild) {
-        gridContainer.removeChild(gridContainer.firstChild);
-    }  // This resets the grid
-
     gridContainer.classList.add('thirtytwo_thirtytwo');  // css class style for grid
 
     for (let numberOfGrids = 1; numberOfGrids <= 32*32; numberOfGrids++) {
         const gridBox = document.createElement('div');
-        gridBox.classList.add('gridBox');
+        gridBox.classList.add('gridBox');  // adds individual grid boxes
 
         gridBox.addEventListener('mouseover', function handleMouseOver() {
             gridBox.style.backgroundColor = chosenColor;
@@ -75,21 +74,17 @@ const THIRTYTWO_THIRTYTWO = () => {
 
 const SIXTYFOUR_SIXTYFOUR = () => {
 
-    while (gridContainer.firstChild) {
-        gridContainer.removeChild(gridContainer.firstChild);
-    }  // This resets the grid
-
     gridContainer.classList.add('sixtyfour_sixtyfour');  // css class style for grid
 
     for (let numberOfGrids = 1; numberOfGrids <= 64*64; numberOfGrids++) {
         const gridBox = document.createElement('div');
-        gridBox.classList.add('gridBox');
-        gridContainer.appendChild(gridBox);
+        gridBox.classList.add('gridBox');  // adds individual grid boxes
 
         gridBox.addEventListener('mouseover', function handleMouseOver() {
             gridBox.style.backgroundColor = chosenColor;
           });
-
+        
+        gridContainer.appendChild(gridBox);
     }
     
     defaultGrid = SIXTYFOUR_SIXTYFOUR;
@@ -100,17 +95,30 @@ window.onload = defaultGrid;
 
 const reset = document.querySelector('#reset');
 reset.onclick = () => {
+    RESET_GRID();
     defaultGrid();
 };
 
 const eightButton = document.querySelector('#eightButton');
-eightButton.onclick = () => EIGHT_EIGHT();
+eightButton.onclick = () => {
+    RESET_GRID();
+    EIGHT_EIGHT();
+}
 
 const sixteenButton = document.querySelector('#sixteenButton');
-sixteenButton.onclick = () => SIXTEEN_SIXTEEN();
+sixteenButton.onclick = () => {
+    RESET_GRID();
+    SIXTEEN_SIXTEEN();
+}
 
 const thirtytwoButton = document.querySelector('#thirtytwoButton');
-thirtytwoButton.onclick = () => THIRTYTWO_THIRTYTWO();
+thirtytwoButton.onclick = () => {
+    RESET_GRID();
+    THIRTYTWO_THIRTYTWO();
+}
 
 const sixtyfourButton = document.querySelector('#sixtyfourButton');
-sixtyfourButton.onclick = () => SIXTYFOUR_SIXTYFOUR();
+sixtyfourButton.onclick = () => {
+    RESET_GRID();
+    SIXTYFOUR_SIXTYFOUR();
+}
